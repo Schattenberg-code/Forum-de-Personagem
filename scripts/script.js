eada1d7c6f5fb7f8c9e7b247e4e8a8ff6b74f1ee
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-04-27 09:02:04 -0300

    ADD - Redirecionamento automático com detecção de login

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
+if (window.location.pathname.includes("forum.html")) {
+    if (localStorage.getItem("logado") !== "true") {
+        window.location.replace("index.html");
+    }
+}
+
 document.getElementById("form-login").onsubmit = (e) => {
     
     e.preventDefault();
@@ -18,6 +24,7 @@ document.getElementById("form-login").onsubmit = (e) => {
 
     if(valido == true){
         console.log("Validado");
+        localStorage.setItem("logado", "true");
         window.location.replace("forum.html");
     }else{
         console.log("Inválido");
