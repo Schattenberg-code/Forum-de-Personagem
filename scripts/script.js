4a0a42929ad8a011965fae591a19ac99af435e1c
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-04-27 08:18:57 -0300

    ADD - começo da validação de login

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,29 @@
-usuario = document.getElementById('usuario').value
-senha = document.getElementById('senha').value
+document.getElementById("form-login").onsubmit = (e) => {
+    
+    e.preventDefault();
+
+    console.log("OI");
+
+    let usuario = document.getElementById('usuario').value
+    let senha = document.getElementById('senha').value
+
+    let valido = true;
+
+    if (usuario !== 'admin'){
+        valido = false;
+        return;
+    }
+    if (senha !== '123'){
+        valido = false;
+        return;
+    }
+
+    if(valido == true){
+        console.log("Validado");
+    }else{
+        console.log("Inválido");
+    }
+}
+
+
 
