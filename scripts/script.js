3c9d168d23d6cee9019c9ebdc010442facd92d4f
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-04-29 11:49:00 -0300

    Fix - Bots só respondem 1 vez

--- scripts/script.js
+++ scripts/script.js
@@ -132,7 +132,7 @@ if (form_envio) {
 
             form_envio.reset();
 
-            if (todosComentarios.length >= 1) {
+            if (todosComentarios.length == 6) {
                 let todosComentarios = document.querySelectorAll('.comentario_alheio');
                 let ultimoComentario = todosComentarios[todosComentarios.length - 1];
                 setTimeout(() => {
