93ae40a67bd0ea0b1e1a54fd1dc9a730c5578e83
Schattenberg-code <lucas_schattenberg@estudante.sesisenai.org.br>
2026-04-29 11:32:10 -0300

    add - mais customização na aba de comentarios

--- scripts/script.js
+++ scripts/script.js
@@ -71,34 +71,34 @@ if (form_login) {
 const form_envio = document.querySelector('#mandar_comentario');
 
 if (form_envio) {
-    form_envio.addEventListener('submit', function(e) {
+    form_envio.addEventListener('submit', function (e) {
         e.preventDefault();
-        
+
         const textoDigitado = form_envio.querySelector('textarea').value;
 
-        if(textoDigitado !== ''){
-           const todosComentarios = document.querySelectorAll('.comentario_alheio');
-        const ultimoComentario = todosComentarios[todosComentarios.length - 1];
+        if (textoDigitado !== '') {
+            const todosComentarios = document.querySelectorAll('.comentario_alheio');
+            const ultimoComentario = todosComentarios[todosComentarios.length - 1];
 
-        const novoComentarioHTML = `
+            const novoComentarioHTML = `
             <div class="comentario_alheio">
-                <div class="d-flex">
+                <div class="d-flex comentario_meu">
                     <img class="circulo" src="assets/images/avatar_comentario.png" alt="Foto do Ezio">
                     <textarea disabled class="comentario">${textoDigitado}</textarea>
                 </div>
             </div>
         `;
 
-        console.log(todosComentarios);
+            console.log(todosComentarios);
 
-        if (ultimoComentario) {
-            ultimoComentario.insertAdjacentHTML('afterend', novoComentarioHTML);
+            if (ultimoComentario) {
+                ultimoComentario.insertAdjacentHTML('afterend', novoComentarioHTML);
+            }
+
+            form_envio.reset();
         }
 
-        form_envio.reset(); 
-        }
 
-        
     });
 }
 
